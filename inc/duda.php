2367664bbe8843b6bdff0e386ae3900d6e4c0e90
Edward Alarco <edwarddac.56@gmail.com>
2023-06-19 11:28:42 -0500

    inicio responsive

--- inc/duda.php
+++ inc/duda.php
@@ -6,7 +6,7 @@
                 <p><?php echo get_field('descripcion_duda', 'option') ?></p>
             </div>
             <div class="col">
-                <a class="dude_box" href="<?php echo esc_url(home_url('contactanos')) ?>">
+                <a class="dude_box" href="<?php echo esc_url(home_url('contactanos')) ?>" title="Contacto">
                     <div>
                         <svg width="125" height="125" viewBox="0 0 125 125" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <g id="duda">
